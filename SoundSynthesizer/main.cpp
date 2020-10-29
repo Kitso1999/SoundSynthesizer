6e32ec92ed5df44cda3e52a99a6052984d4eb414
kitso1999 <kitso1999@gmail.com>
2020-10-29 04:00:40 +0400

    Added Oscilators and defined amplitude variable, but it didn't serve its purpose well and may be deleted in the future

--- SoundSynthesizer/main.cpp
+++ SoundSynthesizer/main.cpp
@@ -3,10 +3,47 @@
 #include "olcNoiseMaker.h"
 
 std::atomic<double> dFrequencyOutput = 0.0;
+double dOctaveBaseFrequency = 110.0;
+double d12thRootOf2 = std::pow(2.0, 1.0 / 12.0);
+double dAmp = 0.0;
+
+
+//Convert Frequency to Angular Frequency
+double w(double dHertz)
+{
+	return 2 * PI * dHertz;
+}
+
+double osc(double dHertz, double dTime, int nType)
+{
+	switch (nType)
+	{
+	case 0: //Sine Wave
+		return sin(w(dHertz) * dTime);
+	case 1:	//Square Wawe
+		return (sin(w(dHertz) * dTime) > 0) ? 1.0 : -1.0;
+	case 2:	//Triangle Wawe
+		return (2 / PI) * asin(sin(w(dHertz) * dTime));
+	case 3:	//Saw Wave Math Method
+	{
+		double dOutput = 0.0;
+
+		for (double n = 1.0; n < 100.0; ++n)
+			dOutput += (sin(n * w(dHertz) * dTime)) / n;
+		return dOutput * (2.0 / PI);
+	}
+	case 4:	//Saw Wave CS Method
+		return (2.0 / PI) * (dHertz * PI * fmod(dTime, 1.0 / dHertz) - (PI / 2.0));
+	case 5: //Pseudo Random Noice
+		return 2.0 * (double)rand() / (double)RAND_MAX - 1.0;
+		
+	default: return 0.0;
+	}
+}
 
 double MakeNoice(double dTime)
 {
-	double dOutput = sin(dFrequencyOutput * 2.0 * 3.14159 * dTime);
+	double dOutput = dAmp * osc(dFrequencyOutput, dTime, 3);
 	return dOutput;
 }
 
@@ -25,8 +62,6 @@ int main()
 	olcNoiseMaker<short> sound(devices[0], 44100, 1, 8, 512);
 	sound.SetUserFunction(MakeNoice);
 
-	double dOctaveBaseFrequency = 110.0;
-	double d12thRootOf2 = std::pow(2.0, 1.0 / 12.0);
 
 
 	
@@ -38,14 +73,15 @@ int main()
 		{
 			if (GetAsyncKeyState((unsigned char)("ZSXDCVGBHNJMK\xbcL\xbe"[k])) & 0x8000)
 			{
-				
 				dFrequencyOutput = dOctaveBaseFrequency * std::pow(d12thRootOf2, k);
 				bKeyPressed = true;
+				dAmp = (dAmp + 0.01 < 1.0) ? dAmp + 0.01 : 1.0;
 			}
+	
 		}
 		if (!bKeyPressed)
 		{
-			dFrequencyOutput = 0.0;
+			dAmp = (dAmp - 0.01 > 0.0) ? dAmp - 0.01 : 0.0;
 		}
 	}
 
