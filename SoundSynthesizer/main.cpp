23b7c2c0cb9181433307678ac5c68eb71cac07c6
kitso1999 <kitso1999@gmail.com>
2020-10-30 04:36:50 +0400

    Added Envelopes

--- SoundSynthesizer/main.cpp
+++ SoundSynthesizer/main.cpp
@@ -2,11 +2,6 @@
 
 #include "olcNoiseMaker.h"
 
-std::atomic<double> dFrequencyOutput = 0.0;
-double dOctaveBaseFrequency = 110.0;
-double d12thRootOf2 = std::pow(2.0, 1.0 / 12.0);
-double dAmp = 0.0;
-
 
 //Convert Frequency to Angular Frequency
 double w(double dHertz)
@@ -41,11 +36,105 @@ double osc(double dHertz, double dTime, int nType)
 	}
 }
 
+
+struct sEnvelopeADSR
+{
+	double dAttackTime;
+	double dDecayTime;
+	double dReleaseTime;
+
+	double dSustainAmplitude;
+	double dStartAmplitude;
+
+	double dTriggerOnTime;
+	double dTriggerOffTime;
+
+	bool bNoteOn;
+
+	sEnvelopeADSR()
+	{
+		dAttackTime = 0.01;
+		dDecayTime = 0.01;
+		dStartAmplitude = 1.0;
+		dSustainAmplitude = 0.8;
+		dReleaseTime = 0.02;
+		dTriggerOnTime = 0.0;
+		dTriggerOffTime = 0.0;
+		bNoteOn = false;
+	}
+
+	double GetAmplitude(double dTime)
+	{
+		double dAmplitude = 0.0;
+		double dLifeTime = dTime - dTriggerOnTime;
+
+
+		if (bNoteOn)
+		{
+			//ADS
+
+			//Attack
+			if (dLifeTime <= dAttackTime)
+				dAmplitude = (dLifeTime / dAttackTime) * dStartAmplitude;
+
+			//Decay
+			if (dLifeTime > dAttackTime && dLifeTime <= (dAttackTime + dDecayTime))
+			{
+				dAmplitude = dStartAmplitude - (dLifeTime - dAttackTime) / dDecayTime * ((dStartAmplitude - dSustainAmplitude));
+			}
+
+			//Sustain
+			if (dLifeTime > (dAttackTime + dDecayTime))
+			{
+
+				dAmplitude = dSustainAmplitude;
+			}
+		}
+		else
+		{
+			//R
+			dAmplitude = dSustainAmplitude * ( 1 - ((dTime - dTriggerOffTime) / dReleaseTime));
+		}
+		
+		if (dAmplitude < 0.0001)
+		{
+			dAmplitude = 0.0;
+		}
+
+
+
+
+		return dAmplitude;
+	}
+
+	void NoteOn(double dTimeOn)
+	{
+		dTriggerOnTime = dTimeOn;
+		bNoteOn = true;
+	}
+
+	void NoteOff(double dTimeOff)
+	{
+		dTriggerOffTime = dTimeOff;
+		bNoteOn = false;
+	}
+};
+
+
+//Global Synthesizer Variables
+std::atomic<double> dFrequencyOutput = 0.0;
+sEnvelopeADSR envelope;
+double dOctaveBaseFrequency = 110.0;
+double d12thRootOf2 = std::pow(2.0, 1.0 / 12.0);
+double dAmp = 0.0;
+
+
 double MakeNoice(double dTime)
 {
-	double dOutput = dAmp * osc(dFrequencyOutput, dTime, 3);
+	double dOutput = envelope.GetAmplitude(dTime) * osc(dFrequencyOutput, dTime, 2);
 	return dOutput;
 }
+
 
 
 int main()
@@ -63,25 +152,35 @@ int main()
 	sound.SetUserFunction(MakeNoice);
 
 
-
+	int nCurrentKey = -1;
+	bool bKeyPressed;
 	
 	while (true)
 	{
-		bool bKeyPressed = false;
+		bKeyPressed = false;
 		//Add a Keyboard
 		for (int k = 0; k < 16; ++k)
 		{
 			if (GetAsyncKeyState((unsigned char)("ZSXDCVGBHNJMK\xbcL\xbe"[k])) & 0x8000)
 			{
-				dFrequencyOutput = dOctaveBaseFrequency * std::pow(d12thRootOf2, k);
+				if (nCurrentKey != k)
+				{
+					dFrequencyOutput = dOctaveBaseFrequency * std::pow(d12thRootOf2, k);
+					envelope.NoteOn(sound.GetTime());
+					bKeyPressed = true;
+					nCurrentKey = k;
+				}
 				bKeyPressed = true;
-				dAmp = (dAmp + 0.01 < 1.0) ? dAmp + 0.01 : 1.0;
 			}
 	
 		}
 		if (!bKeyPressed)
 		{
-			dAmp = (dAmp - 0.01 > 0.0) ? dAmp - 0.01 : 0.0;
+			if (nCurrentKey != -1)
+			{
+				envelope.NoteOff(sound.GetTime());
+				nCurrentKey = -1;
+			}
 		}
 	}
 
